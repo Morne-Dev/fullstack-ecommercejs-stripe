72d5d560529ab4dbfe4bb741d85607787a093cad
Morne Marais <morne.dev114@gmail.com>
2021-07-25 14:16:16 +0200

    Fix: components, intial Basket

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,42 @@ import { commerce } from "./lib/commerce";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Products from "./components/Products";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  
+  const [basketData, setBasketData] = useState({});
+
   const fetchProducts = async () => {
     const response = await commerce.products.list();
     setProducts((response && response.data) || []);
   };
 
+  const fetchBasketData = async () => {
+    const response = await commerce.cart.retrieve();
+    setBasketData(response);
+  };
+
+  const addProduct = async (productId, quantity) => {
+    const response = await commerce.cart.add(productId, quantity);
+    setBasketData(response.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchBasketData();
   }, []);
 
   return (
     <Router>
       <div>
-        <NavBar />
+        <NavBar basketItems={basketData.total_items}/>
         <Switch>
           <Route exact path="/">
-            <Products products={products} />
+            <Products products={products} addProduct={addProduct} />
           </Route>
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,9 +1,9 @@
 import { Grid, Container } from "@material-ui/core";
 import Product from "../Product";
-import Banner from "../Banner"
+import Banner from "../Banner";
+import "./style.css";
 
-
-const Products = ({products}) => {
+const Products = ({products, addProduct}) => {
 console.log("products ===", products);
 
   return (
@@ -14,7 +14,7 @@ console.log("products ===", products);
           {products.map((product) => {
             return (
             <Grid key={product.id} item xs={12} sm={6} md={4}>
-            <Product product={product} />;
+            <Product product={product} addProduct={addProduct}/>
             </Grid>
             );
           })}
